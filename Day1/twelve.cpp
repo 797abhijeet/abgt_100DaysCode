23517b58e96a9217dde5c4f800e9de0007361a3e
797abhijeet <abhijeetsaxena615@gmail.com>
2025-11-18 14:41:14 +0530

    q solved

---
+++ Day1/twelve.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int longestSubarray(vector<int>& arr, int k) {
+        // code here
+        int n=arr.size();
+        unordered_map<int,int>mp;
+        mp[0]=-1;
+        
+        int sum=0;
+        int ans=0;
+        
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            
+            if(mp.find(sum-k)!=mp.end()){
+                ans=max(ans,i-mp[sum-k]);
+            }
+            if(mp.find(sum)==mp.end()) mp[sum]=i;
+        }
+        return ans;
+    }
+};
