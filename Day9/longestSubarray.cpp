d4ca09ab990eed2d8a00cc3755c799c4a903c578
797abhijeet <abhijeetsaxena615@gmail.com>
2025-11-29 13:17:46 +0530

    longest Subarray with sum k

---
+++ Day9/longestSubarray.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int longestSubarray(vector<int> &arr, int k)
+    {
+        // code here
+        map<int, int> mp;
+        mp[0] = -1;
+        int sum = 0;
+        int ans = 0;
+
+        for (int i = 0; i < arr.size(); i++)
+        {
+            sum += arr[i];
+
+            if (mp.find(sum - k) != mp.end())
+            {
+                ans = max(ans, i - mp[sum - k]);
+            }
+            if (mp.find(sum) == mp.end())
+            {
+                mp[sum] = i;
+            }
+        }
+        return ans;
+    }
+};
