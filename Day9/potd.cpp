684c4a48e2e8fb7371212fb89e497600df158a7f
797abhijeet <abhijeetsaxena615@gmail.com>
2025-11-29 13:13:32 +0530

    DAY 10 ADDED:

---
+++ Day9/potd.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int minOperations(vector<int> &nums, int k)
+    {
+        int sum = 0;
+        for (auto it : nums)
+        {
+            sum += it;
+        }
+        return sum % k;
+    }
+};
